0c7d71270ada608fc238b1adc4ab1a985fb29b11
Fabricio Araujo <fabricio_2310@hotmail.com>
2022-10-18 16:33:50 +0100

    Resolvido ex_2 da lista 4

--- lista_ex_4/ex_2.py
+++ lista_ex_4/ex_2.py
@@ -1,3 +1,19 @@
 """ 2. Sorteie 20 inteiros entre 1 e 100 numa lista.
 Armazene os números pares na lista PAR e os números ímpares na lista IMPAR.
 Imprima as três listas. """
+
+import random
+
+sort_20_int = random.sample(range(100), 20)
+
+PAR = []
+IMPAR = []
+
+for index in sort_20_int:
+    if index % 2 == 0:
+        PAR.append(index)
+    else:
+        IMPAR.append(index)
+
+print('PAR:', PAR)
+print('IMPAR:', IMPAR)
