994db3509ec3b81abda7eb0f6ddc5a4ee4113249
Fabricio Araujo <fabricio_2310@hotmail.com>
2022-10-19 15:24:46 +0100

    Resolvido ex_4 da lista 4

--- lista_ex_4/ex_4.py
+++ lista_ex_4/ex_4.py
@@ -9,3 +9,31 @@ as palavras que começam ou terminam com uma das letras “python”.
 Imprima a lista resultante.
 Não se esqueça de remover antes os caracteres especiais
 e cuidado com maiúsculas e minúsculas. """
+
+texto = """
+The Python Software Foundation and the global Python community welcome
+and encourage participation by everyone. Our community is based on mutual
+respect, tolerance, and encouragement, and we are working to help each
+other live up to these principles. We want our community to be more diverse:
+whoever you are, and whatever your background, we welcome you.
+"""
+
+texto = texto.replace(',', ' ',)
+texto = texto.replace('.', ' ',)
+texto = texto.replace(':', ' ',)
+texto = texto.lower()
+
+lista = texto.split()
+
+python = list('python')
+
+lista_palavras = []
+
+for index in lista:
+    for letra in python:
+        if index[0] == letra:
+            lista_palavras.append(index)
+        elif index[-1] == letra:
+            lista_palavras.append(index)
+
+print(lista_palavras)
