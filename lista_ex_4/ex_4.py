7baf80aa5c6d0cced2023631ea035b9a2393c739
Fabricio Araujo <fabricio_2310@hotmail.com>
2022-10-20 15:04:21 +0100

    Correçao dos exercícios 4 e 5 da lista 4

--- lista_ex_4/ex_4.py
+++ lista_ex_4/ex_4.py
@@ -10,30 +10,32 @@ Imprima a lista resultante.
 Não se esqueça de remover antes os caracteres especiais
 e cuidado com maiúsculas e minúsculas. """
 
+from string import punctuation
+
+
 texto = """
 The Python Software Foundation and the global Python community welcome
 and encourage participation by everyone. Our community is based on mutual
 respect, tolerance, and encouragement, and we are working to help each
 other live up to these principles. We want our community to be more diverse:
 whoever you are, and whatever your background, we welcome you.
-"""
+""".lower()
 
-texto = texto.replace(',', ' ',)
-texto = texto.replace('.', ' ',)
-texto = texto.replace(':', ' ',)
-texto = texto.lower()
-
-lista = texto.split()
-
-python = list('python')
+for caract_esp in punctuation:
+    texto = texto.replace(caract_esp, ' ')
 
 lista_palavras = []
 
-for index in lista:
-    for letra in python:
-        if index[0] == letra:
-            lista_palavras.append(index)
-        elif index[-1] == letra:
-            lista_palavras.append(index)
+for palavra in texto.split():
+    if palavra[0] in 'python' or palavra[-1] in 'python':
+        lista_palavras.append(palavra)
 
+# Com list comprehension
+resp = []
+
+resp = [p for p in texto.split()
+        if p[0] in 'python' or p[-1] in 'python']
+
+print(resp)
+print()
 print(lista_palavras)
--- lista_ex_4/ex_5.py
+++ lista_ex_4/ex_5.py
@@ -2,3 +2,42 @@
 uma das letras “python” e que tenham mais de 4 caracteres. Não se esqueça de
 transformar maiúsculas para minúsculas e de remover
 antes os caracteres especiais. """
+
+from string import punctuation
+
+
+texto = """
+The Python Software Foundation and the global Python community welcome
+and encourage participation by everyone. Our community is based on mutual
+respect, tolerance, and encouragement, and we are working to help each
+other live up to these principles. We want our community to be more diverse:
+whoever you are, and whatever your background, we welcome you.
+""".lower()
+
+# Tratamento da string -> remove caracteres especiais (pontuações)
+for carac_esp in punctuation:  # punctuation -> funçaoi do método string
+    texto = texto.replace(carac_esp, ' ')
+
+# Funçao que verifica se palavra possui alguma letra dentro da palavra 'python'
+def pytonica(palavra):
+    for letra in palavra:
+        if letra in 'python':
+            return True
+    return False
+
+
+resp = []
+# Adiciona na lista as palavra que contém alguma letra de 'python'
+for palavra in texto.split():
+    if pytonica(palavra) and len(palavra) > 4:
+        resp.append(palavra)
+
+print(resp)
+print(len(resp))
+
+# Com list Comprehension
+resp2 = [palavra for palavra in texto.split()
+        if pytonica(palavra) and len(palavra) > 4]
+
+print (resp2)
+print (len(resp2))
