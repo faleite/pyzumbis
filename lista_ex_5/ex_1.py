1b75e6b5905eb611b41de24243b8b1b53e80328b
Fabricio Araujo <fabricio_2310@hotmail.com>
2022-10-21 12:54:17 +0100

    Adicionado lista de exercicios 5

---
+++ lista_ex_5/ex_1.py
@@ -0,0 +1,13 @@
+""" Google Developer Day 2010
+Questão A. O que o seguinte programa (dado na forma de pseucódigo) imprime?
+
+x = 2
+y = 5
+se y > 8 então
+y = y * 2
+caso contrário,
+x = x * 2
+imprime (x + y)
+Resposta: 9
+
+"""
---
+++ lista_ex_5/ex_4.py
@@ -0,0 +1,10 @@
+""" Google Developer Day 2010
+
+Questão D.
+Daniela é uma pessoa muito supersticiosa. Para ela, um número é sortudo se
+ele contém o dígito 2 mas não o dígito 7. Então, na opinião dela, quantos
+números sortudos existem entre 18644 e 33087, incluindo os extremos?
+
+Resposta: 7995
+
+"""
