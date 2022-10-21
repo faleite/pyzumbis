0428c50b10af891a207df90b88de59b8dbc52971
Fabricio Araujo <fabricio_2310@hotmail.com>
2022-10-21 13:02:00 +0100

    Adicionado enunciado 2 e 3 da lista de exercicios 5

---
+++ lista_ex_5/ex_2.py
@@ -0,0 +1,13 @@
+""" Google Developer Day 2010
+Questão B. Quantas vezes o trecho de pseudocódigo seguinte imprime 'oi'?
+(obs: na nossa pseudo-linguagem, o laço inclui os extremos, ou seja, 1
+até 4 significa 1, 2, 3, 4.)
+
+para i = 1 até 9
+    se i != 3, então
+        para j = 1 até 6
+            imprime 'oi'
+
+Resposta: 48
+
+"""
---
+++ lista_ex_5/ex_3.py
@@ -0,0 +1,8 @@
+""" Google Developer Day 2010
+
+Questão C. Entre 1067 e 3627 (inclusive),
+quantos números são pares e também divisíveis por 7?
+
+Resposta: 183
+
+"""
